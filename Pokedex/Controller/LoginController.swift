993c8ad2a6a40f4ccd6979e33704a248ce76c114
Emiliogn98 <Emiliogn.98@gmail.com>
2023-07-03 17:56:42 -0600

    Face ID

--- Pokedex/Controller/LoginController.swift
+++ Pokedex/Controller/LoginController.swift
@@ -1,11 +1,16 @@
 
 
 import UIKit
+import LocalAuthentication
+import Security
+
+
 
 class LoginController: UIViewController {
     var listaUsers : [User1] = []
     var color = UIColor.red.cgColor
         var color2 = UIColor.white.cgColor
+    var contrasenia : String = ""
  
     
     
@@ -73,6 +78,19 @@ class LoginController: UIViewController {
         btnLogin.tintColor = UIColor(named: "grisbotonlogin")
            return btnLogin
        }()
+    private lazy var btnFaceId : UIButton = {
+        var config = UIButton.Configuration.borderedProminent()
+        config.title = "FaceID"
+        let btnLogin = UIButton(type: .system)
+        btnLogin.addTarget(self, action: #selector(btnFaceIDAction), for: .touchUpInside)
+        btnLogin.configuration = config
+        btnLogin.translatesAutoresizingMaskIntoConstraints = false
+        btnLogin.setTitleColor(UIColor.white, for: .normal)
+        btnLogin.layer.cornerRadius = 0
+        btnLogin.layer.masksToBounds = true
+        btnLogin.tintColor = UIColor(named: "grisbotonlogin")
+           return btnLogin
+       }()
     private let lblValidacion : UILabel = {
           let lblValidacionLogin = UILabel()
         lblValidacionLogin.numberOfLines = 0
@@ -96,7 +114,7 @@ class LoginController: UIViewController {
    
 
         
-        [ImageView,txtUserName,btnLogin,txtPassword,lblValidacion,ImageViewPokemon].forEach(view.addSubview)
+        [ImageView,txtUserName,btnLogin,txtPassword,lblValidacion,ImageViewPokemon,btnFaceId].forEach(view.addSubview)
         view.bringSubviewToFront(ImageView)
        
         /* contraints*/
@@ -127,6 +145,11 @@ class LoginController: UIViewController {
             ImageViewPokemon.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -600),
             ImageViewPokemon.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -30),
             ImageViewPokemon.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 30),
+            
+            btnFaceId.topAnchor.constraint(equalTo: lblValidacion.bottomAnchor, constant: 20),
+            btnFaceId.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -140),
+            btnFaceId.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -50),
+            btnFaceId.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 47),
       
         
 //
@@ -134,7 +157,83 @@ class LoginController: UIViewController {
     
        
 
+        
     }
+    
+    func isFaceIDAvailable() -> Bool {
+        let context = LAContext()
+        var error: NSError?
+        
+        return context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+    }
+    func authenticateWithFaceID(completion: @escaping (Bool) -> Void) {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Autenticación requerida") { (success, error) in
+                DispatchQueue.main.async {
+                    completion(success)
+                    
+                    
+                }
+            }
+        } else {
+            // Face ID no está disponible
+            completion(false)
+        }
+    }
+    func saveToKeychain(_ identifier: String,_ password: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: identifier,
+            kSecValueData as String: password.data(using: .utf8)!,
+            kSecAttrAccessible as String: kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            print("Error al guardar en Keychain")
+            return
+        }
+        
+        print("Guardado exitoso en Keychain")
+    }
+    func readFromKeychain(_ identifier: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: identifier,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        
+        guard status == errSecSuccess, let result = dataTypeRef as? Data else {
+            print("Error al leer desde Keychain")
+            return nil
+        }
+        
+        let password = String(data: result, encoding: .utf8)
+        return password
+    }
+    @objc func btnFaceIDAction(){
+        let context = LAContext()
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "autenticacion requerida") { sucess, error in
+            if sucess == true {
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "LoginGetSegue", sender: self)
+                        
+                }
+              
+            }else{
+                
+            }
+        }
+        
+        }
+    
     
     @objc func btnLoginAction() {
         guard txtUserName.text != "" else{
@@ -148,17 +247,17 @@ class LoginController: UIViewController {
         txtUserName.layer.borderColor = color2
         txtUserName.layer.borderWidth = 1.0
                
-             guard txtPassword.text != "" else{
-                 lblValidacion.text = "Campo Requerido"
-                 lblValidacion.textColor  = .red
-                 txtPassword.layer.borderColor = color
-                 txtPassword.layer.borderWidth = 1.0
-                 return
-                    }
-        lblValidacion.text = ""
-                 
-        txtPassword.layer.borderColor = color2
-        txtPassword.layer.borderWidth = 1.0
+//             guard txtPassword.text != "" else{
+//                 lblValidacion.text = "Campo Requerido"
+//                 lblValidacion.textColor  = .red
+//                 txtPassword.layer.borderColor = color
+//                 txtPassword.layer.borderWidth = 1.0
+//                 return
+//                    }
+//        lblValidacion.text = ""
+//
+//        txtPassword.layer.borderColor = color2
+//        txtPassword.layer.borderWidth = 1.0
         
         var result = LoginViewModel.GetByUsername(txtUserName.text!)
         if result.Correct == true {
@@ -169,6 +268,14 @@ class LoginController: UIViewController {
                             listaUsers.append(user)
                        }
             print(listaUsers)
+     //       readFromKeychain(listaUsers[0].username!)
+            if let password = readFromKeychain(listaUsers[0].username!){
+                self.contrasenia = password
+              
+                txtPassword.text = self.contrasenia
+                self.contrasenia = txtPassword.text!
+             //   self.performSegue(withIdentifier: "LoginGetSegue", sender: self)
+            }
             
             guard txtPassword.text == listaUsers[0].password else {
                 let alert = UIAlertController(title: "Mensaje", message: "Contraseña incorrecta", preferredStyle: .alert)
@@ -177,15 +284,14 @@ class LoginController: UIViewController {
                               
                                   self.present(alert, animated: true)
                 
-              //  lblValidacion.text = "Contraseña incorrecta"
-               // lblValidacion.textColor  = .red
+             
                 
                 return
             }
             lblValidacion.text = ""
+            saveToKeychain(listaUsers[0].username!, listaUsers[0].password!)
             self.performSegue(withIdentifier: "LoginGetSegue", sender: self)
-            
-            
+        
             
         }else {
             let alert = UIAlertController(title: "Mensaje", message: "Ocurrio un error al ingresar, el nombre de usuario no existe.", preferredStyle: .alert)
